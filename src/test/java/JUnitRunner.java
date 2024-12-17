105921e5e1c74e2327a3fe1b40dee85e001234f1
Gaurav Kumar <Gk225260@gmail.com>
2024-12-17 10:48:44 +0530

    Changes

---
+++ src/test/java/JUnitRunner.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        glue = {"Steps"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        monochrome = true
+)
+public class JUnitRunner {
+
+
+
+}
